29d1b3d077955b2f049f61469bd4ae8b9b13cf1c
forKnowYou <1003425554@qq.com>
2018-05-21 16:43:54 +0800

    better lib

--- cTools/cTools.h
+++ cTools/cTools.h
@@ -8,6 +8,22 @@ extern "C" {
 #include "stdio.h"
 #include "stdint.h"
 
+#define MAX_IN_ARRAY(buf, len, rslt, sub) \
+          rslt = buf[0]; \
+          for(int var_i = 1; var_i < len; var_i ++) { \
+            if(rslt < buf[var_i]) \
+              rslt = buf[var_i]; \
+              sub = var_i; \
+          }
+          
+#define MIN_IN_ARRAY(buf, len, rslt, sub) \
+          rslt = buf[0]; \
+          for(int var_i = 1; var_i < len; var_i ++) { \
+            if(rslt > buf[var_i]) \
+              rslt = buf[var_i]; \
+              sub = var_i; \
+          }
+
 typedef int       (*pfPrint_t)(const char* pCh);
 
 typedef struct {
@@ -16,25 +32,9 @@ typedef struct {
   pfPrint_t   pfPrint;
 } sCPrintf_t;
 
-typedef enum {
-  eBASETYPE_INT8,
-  eBASETYPE_UINT8,
-  eBASETYPE_INT16,
-  eBASETYPE_UINT16,
-  eBASETYPE_INT32,
-  eBASETYPE_UINT32,
-  eBASETYPE_FLOAT,
-  eBASETYPE_INT64,
-  eBASETYPE_UINT64,
-  eBASETYPE_DOUBLE
-} eBaseType_t;
-
 void        cPrintfFlush(sCPrintf_t* sCP);
 void        cPrintfInit(sCPrintf_t* sCP, char* pBuf, pfPrint_t pfPrint);
 void        cPrintfToBuf(sCPrintf_t* sCP, char ch);
-
-char*       cFindArrayMaxAddr(char* pBuf, eBaseType_t size, uint16_t len);
-char*       cFindArrayMinAddr(char* pBuf, eBaseType_t size, uint16_t len);
 
 extern sCPrintf_t        sCPrintf;
 
